9aeacaa194722d8ae6d54612bd3009763df6140a
abbasnake <mr.rihards.ozols@gmail.com>
2018-04-16 09:40:28 +0300

    added views folder and setup two view files

---
+++ client/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>hello im home view</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  background-color: pink;
+}
+</style>
---
+++ client/src/views/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>hello im statistics view</h1>
+</template>
+
+<script>
+export default {
+  name: 'Statistics'
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  background-color: aqua;
+}
+</style>
