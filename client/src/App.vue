387b08c0c0b1a1ee0fac27e67f0ef45d66df8afc
abbasnake <mr.rihards.ozols@gmail.com>
2018-04-16 10:22:12 +0300

    added scss folder for css reset and variables

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,13 +10,12 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<!-- GLOBAL STYLE SETTINGS -->
+<style lang="scss">
+@import './scss/reset';
+</style>
+
+<!-- LOCAL STYLE SETTINGS -->
+<style lang="scss" scoped>
+
 </style>
