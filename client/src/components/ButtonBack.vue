d789588a38e4321caabca6402e599f42425f1404
abbasnake <mr.rihards.ozols@gmail.com>
2018-04-16 13:24:59 +0300

    building icon-arrow and button-back components

--- client/src/components/ButtonBack.vue
+++ client/src/components/ButtonBack.vue
@@ -1,13 +1,31 @@
 <template>
-  <button>&lt;</button>
+  <button class="button" @click="onClick">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
 export default {
-  name: 'ButtonBack'
+  name: 'ButtonBack',
+  methods: {
+    onClick () {
+      console.log('I have been clicked')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .button {
+    background-color: rgba(100, 100, 100, 0.5);
+    border: none;
+    border-radius: 50%;
+    height: 45px;
+    margin: 10px;
+    padding: 10px;
+    width: 45px;
+    &:hover {
+      fill: #ffffff;
+    }
+  }
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <button-back></button-back>
+  <div class="view">
+    <button-back>
+      <icon-arrow></icon-arrow>
+    </button-back>
     <div>TITLE</div>
     <div>SUBTITLE</div>
     <hr>
@@ -11,18 +13,19 @@
 </template>
 
 <script>
+import IconArrow from '@/components/IconArrow'
 import ButtonBack from '@/components/ButtonBack'
 
 export default {
   name: 'Home',
   components: {
+    'icon-arrow': IconArrow,
     'button-back': ButtonBack
   }
 }
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  background-color: pink;
-}
+  .view {
+  }
 </style>
