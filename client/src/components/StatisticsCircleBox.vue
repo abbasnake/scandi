2f26f2058dcd4186b632e662c967df286b61e85e
abbasnake <mr.rihards.ozols@gmail.com>
2018-04-17 16:32:40 +0300

    made svg circle from stratch, this part is going to be troubling

--- client/src/components/StatisticsCircleBox.vue
+++ client/src/components/StatisticsCircleBox.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="box">
-    CIRCLE
+    <ellipse-background class="box__background"></ellipse-background> 
+    <span class="box__dollars">$420</span>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'StatisticsCircleBox'
+import EllipseBackground from '@/components/EllipseBackground'
+
+export default {
+  name: 'StatisticsCircleBox',
+  components: {
+    'ellipse-background': EllipseBackground
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  .box {
-    border: 1px solid white;
+.box {
+  border: 1px solid white;
+  &__background {
+    position: absolute;
+    top: 20%;
+    left: 20%;
+    z-index: 0;
   }
+  &__dollars {
+    font-size: 28px;
+    position: absolute;
+    top: 43%;
+    left: 40%;
+  }
+}
 </style>
