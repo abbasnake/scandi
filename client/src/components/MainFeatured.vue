8eae5696f3d19217a0ca401bf8d78ae78cbc769a
abbasnake <mr.rihards.ozols@gmail.com>
2018-04-17 07:26:23 +0300

    made info section

--- client/src/components/MainFeatured.vue
+++ client/src/components/MainFeatured.vue
@@ -2,20 +2,20 @@
   <main class="main">
     <section class="main__title">Who are you and why you're standing here?</section>
     <section class="main__subtitle">It doesn't matter if you're building the next Facebook, or if you're the director of marketing for an industrial company.</section>
-    <section class="main__info">
-      <span class="main__info__by">By</span>
-      Nicholas O`Brian 28 july
-    </section>
+    <main-featured-info class="main__info"></main-featured-info>
     <main-featured-likes class="main__likes"></main-featured-likes>
   </main>
 </template>
 
 <script>
 import MainFeaturedLikes from '@/components/MainFeaturedLikes'
+import MainFeaturedInfo from '@/components/MainFeaturedInfo'
+
 export default {
   name: 'MainFeatured',
   components: {
-    'main-featured-likes': MainFeaturedLikes
+    'main-featured-likes': MainFeaturedLikes,
+    'main-featured-info': MainFeaturedInfo
   }
 }
 </script>
@@ -36,13 +36,6 @@ export default {
     font-size: 18px;
     font-weight: 300;
     padding: 5px 40px 10px 10px;
-  }
-  &__info {
-    padding: 10px;
-    &__by {
-      font-family: $font-heebo;
-      font-weight: 300;
-    }
   }
 }
 </style>
---
+++ client/src/components/MainFeaturedInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="info">
+    <span class="info__by">By</span>
+    <span class="info__author">Nicholas O`Brian</span>
+    <span class="info__date">28 July</span>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MainFeaturedDefault'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/variables';
+
+.info {
+  display: grid;
+  grid-template-columns: 23px auto 60px;
+  padding: 12px;
+  &__by {
+    font-family: $font-heebo;
+    font-weight: 300;
+  }
+}
+</style>
